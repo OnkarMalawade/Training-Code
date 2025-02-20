9b8e5361059b47804a19fb09f2aba23cada52af9
Onkar Malawade <132775903+OnkarMalawade@users.noreply.github.com>
2025-02-20 23:55:17 +0530

    Delete Assignment-11 directory

--- Assignment-11/use-Cotext-Project/src/Child2.tsx
+++
@@ -1,9 +0,0 @@
-import Child3 from "./Child3"
-
-const Child2 = () => {
-  return (
-    <div style={{color:"yellow", padding:"30px" ,border:"1px solid blue"}}><Child3 /></div>
-  )
-}
-
-export default Child2
